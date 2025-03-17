bbb66714a5e4c92af2dbe3b85445dd299f728663
Xian <1084C032@ntub.edu.tw>
2025-03-17 09:59:45 +0800

    更新員工資料 更新時間

--- src/App.jsx
+++ src/App.jsx
@@ -280,7 +280,7 @@ function App() {
         return t('nextMondayOrder');
       }
       // 如果是凌晨0點到早上9:30
-      else if (currentTime >= 0 && currentTime <= 11.5) {
+      else if (currentTime >= 0 && currentTime <= 10.5) {
         return t('todayOrder');
       } else {
         return t('tomorrowOrder');
